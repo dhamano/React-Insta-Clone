8ca4679d0960ae938d7ef1662d766b8e12f499e6
Dustin Hamano <dhamano@Dustins-iMac-Pro-2.local>
2019-06-03 15:30:30 -0700

    MPV done

---
+++ instaclone/src/components/PostContainer/PostCard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Card, CardImg } from 'reactstrap';
+
+import PostHeader from './PostHeader';
+import PostInteraction from './PostInteraction';
+import CommentSection from '../CommentSection/CommentSection';
+
+import './postcontainer.scss';
+
+const PostContainer = props => {
+  return (
+    props.cardData.map( (item,i) => {
+      return (
+        <Card key={i}>
+          <PostHeader username={item.username} thumbnail={item.thumbnailUrl} />
+          <CardImg top width="100%" src={item.imageUrl} />
+          <PostInteraction likes={item.likes} />
+          <CommentSection comments={item.comments} timestamp={item.timestamp} />
+        </Card>
+      )
+    })
+  );
+}
+
+export default PostContainer;
